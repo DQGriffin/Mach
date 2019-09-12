fc6ff3270b5d0e3dd7bcf9fa0df64159eb6eddf2
D'Quavius J. Griffin <dquaviusgriffin@gmail.com>
2019-09-12 16:27:15 -0400

    Implement basic support for Windows

--- Mach/src/Mach/System/InputEngine.cpp
+++ Mach/src/Mach/System/InputEngine.cpp
@@ -7,6 +7,7 @@ Mach::InputEngine::InputEngine(int tickRate)
 {
 	this->tickRate = tickRate;
 	isPolling = true;
+	initializeKeyMap();
 }
 
 //==========================================================================
@@ -22,16 +23,25 @@ void Mach::InputEngine::pollInput()
 void Mach::InputEngine::pollKeyInput()
 {
 #if MACH_WINDOWS
-	if (GetKeyState('A') & 0x8000)
+	std::map<Keyboard::Key, int>::iterator iterator = keyMap.begin();
+
+	while (iterator != keyMap.end())
 	{
-		LOG << "a pressed";
-		// Fire an event
-		Mach::Event event;
-		event.keyEvent.key = Keyboard::Key::A;
-		event.type = Event::Type::KeyEvent;
-		Mach::EventManager::fireEvent(event);
+		if (GetKeyState(iterator->second) & 0x8000)
+		{
+			LOG << "Key pressed";
+			Event event;
+			event.keyEvent.key = iterator->first;
+			event.type = Event::Type::KeyEvent;
+			EventManager::fireEvent(event);
+		}
+		iterator++;
 	}
 #endif;
+}
+
+void Mach::InputEngine::pollKeysDown()
+{
 }
 
 //==========================================================================
@@ -52,4 +62,109 @@ void Mach::InputEngine::poll()
 		pollKeyInput();
 		std::this_thread::sleep_for(std::chrono::milliseconds(tickRate));
 	}
+}
+
+//==========================================================================
+// Initialize the key map
+// See pollKeyInput()
+//==========================================================================
+void Mach::InputEngine::initializeKeyMap()
+{
+	// Letters
+	keyMap[Keyboard::Key::A] = 0x41;
+	keyMap[Keyboard::Key::B] = 0x42;
+	keyMap[Keyboard::Key::C] = 0x43;
+	keyMap[Keyboard::Key::D] = 0x44;
+	keyMap[Keyboard::Key::E] = 0x45;
+	keyMap[Keyboard::Key::F] = 0x46;
+	keyMap[Keyboard::Key::G] = 0x47;
+	keyMap[Keyboard::Key::H] = 0x48;
+	keyMap[Keyboard::Key::I] = 0x49;
+	keyMap[Keyboard::Key::J] = 0x4A;
+	keyMap[Keyboard::Key::K] = 0x4B;
+	keyMap[Keyboard::Key::L] = 0x4C;
+	keyMap[Keyboard::Key::M] = 0x4D;
+	keyMap[Keyboard::Key::N] = 0x4E;
+	keyMap[Keyboard::Key::O] = 0x4F;
+	keyMap[Keyboard::Key::P] = 0x50;
+	keyMap[Keyboard::Key::Q] = 0x51;
+	keyMap[Keyboard::Key::R] = 0x52;
+	keyMap[Keyboard::Key::S] = 0x53;
+	keyMap[Keyboard::Key::T] = 0x54;
+	keyMap[Keyboard::Key::U] = 0x55;
+	keyMap[Keyboard::Key::V] = 0x56;
+	keyMap[Keyboard::Key::W] = 0x57;
+	keyMap[Keyboard::Key::X] = 0x58;
+	keyMap[Keyboard::Key::Y] = 0x59;
+	keyMap[Keyboard::Key::Z] = 0x5A;
+
+	// Numbers
+	keyMap[Keyboard::Key::Num0] = 0x30;
+	keyMap[Keyboard::Key::Num1] = 0x31;
+	keyMap[Keyboard::Key::Num2] = 0x32;
+	keyMap[Keyboard::Key::Num3] = 0x33;
+	keyMap[Keyboard::Key::Num4] = 0x34;
+	keyMap[Keyboard::Key::Num5] = 0x35;
+	keyMap[Keyboard::Key::Num6] = 0x36;
+	keyMap[Keyboard::Key::Num7] = 0x37;
+	keyMap[Keyboard::Key::Num8] = 0x38;
+	keyMap[Keyboard::Key::Num9] = 0x39;
+	keyMap[Keyboard::Key::Numpad0] = 0x60;
+	keyMap[Keyboard::Key::Numpad1] = 0x61;
+	keyMap[Keyboard::Key::Numpad2] = 0x62;
+	keyMap[Keyboard::Key::Numpad3] = 0x63;
+	keyMap[Keyboard::Key::Numpad4] = 0x64;
+	keyMap[Keyboard::Key::Numpad5] = 0x65;
+	keyMap[Keyboard::Key::Numpad6] = 0x66;
+	keyMap[Keyboard::Key::Numpad7] = 0x67;
+	keyMap[Keyboard::Key::Numpad8] = 0x68;
+	keyMap[Keyboard::Key::Numpad9] = 0x69;
+
+	// Function Keys
+	keyMap[Keyboard::Key::F1] = 0x70;
+	keyMap[Keyboard::Key::F2] = 0x71;
+	keyMap[Keyboard::Key::F3] = 0x72;
+	keyMap[Keyboard::Key::F4] = 0x73;
+	keyMap[Keyboard::Key::F5] = 0x74;
+	keyMap[Keyboard::Key::F6] = 0x75;
+	keyMap[Keyboard::Key::F7] = 0x76;
+	keyMap[Keyboard::Key::F8] = 0x77;
+	keyMap[Keyboard::Key::F9] = 0x78;
+	keyMap[Keyboard::Key::F10] = 0x79;
+	keyMap[Keyboard::Key::F11] = 0x7A;
+	keyMap[Keyboard::Key::F12] = 0x7B;
+
+	// Action keys
+	keyMap[Keyboard::Key::Backspace] = VK_BACK;
+	keyMap[Keyboard::Key::Tab] = VK_TAB;
+	keyMap[Keyboard::Key::Enter] = VK_RETURN;
+	keyMap[Keyboard::Key::LShift] = VK_LSHIFT;
+	keyMap[Keyboard::Key::RShift] = VK_RSHIFT;
+	keyMap[Keyboard::Key::Menu] = VK_MENU;
+	keyMap[Keyboard::Key::LControl] = VK_LCONTROL;
+	keyMap[Keyboard::Key::RControl] = VK_RCONTROL;
+	keyMap[Keyboard::Key::Pause] = VK_PAUSE;
+	keyMap[Keyboard::Key::Caps] = VK_CAPITAL;
+	keyMap[Keyboard::Key::Escape] = VK_ESCAPE;
+	keyMap[Keyboard::Key::Space] = VK_SPACE;
+	keyMap[Keyboard::Key::PageDown] = VK_PRIOR;
+	keyMap[Keyboard::Key::PageUp] = VK_NEXT;
+	keyMap[Keyboard::Key::End] = VK_END;
+	keyMap[Keyboard::Key::Home] = VK_HOME;
+	keyMap[Keyboard::Key::Left] = VK_LEFT;
+	keyMap[Keyboard::Key::Right] = VK_RIGHT;
+	keyMap[Keyboard::Key::Up] = VK_UP;
+	keyMap[Keyboard::Key::Down] = VK_DOWN;
+	keyMap[Keyboard::Key::Insert] = VK_INSERT;
+	keyMap[Keyboard::Key::Delete] = VK_DELETE;
+	keyMap[Keyboard::Key::LSystem] = VK_LWIN;
+	keyMap[Keyboard::Key::RSystem] = VK_RWIN;
+	keyMap[Keyboard::Key::LBracket] = VK_OEM_4;
+	keyMap[Keyboard::Key::RBracket] = VK_OEM_6;
+	keyMap[Keyboard::Key::Quote] = VK_OEM_7;
+	keyMap[Keyboard::Key::Multiply] = VK_MULTIPLY;
+	keyMap[Keyboard::Key::Divide] = VK_DIVIDE;
+	keyMap[Keyboard::Key::Subtract] = VK_SUBTRACT;
+	keyMap[Keyboard::Key::Add] = VK_ADD;
+	keyMap[Keyboard::Key::Period] = VK_OEM_PERIOD;
 }
--- Mach/src/Mach/System/InputEngine.h
+++ Mach/src/Mach/System/InputEngine.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <chrono>
+#include <map>
 #include "..//MachAPI.h"
 #include "..//Logging/Logger.h"
 #include "Keyboard.h"
@@ -16,17 +17,20 @@ namespace Mach
 	{
 	public:
 	private:
-		std::vector<Keyboard::Key> keysPressed;
+		std::vector<Keyboard::Key> keysDown;		
+		std::map<Keyboard::Key, int> keyMap;
 		int tickRate; // The update rate in ms
 		bool isPolling;
-		
 	public:
-		InputEngine(int tickRate = 200);
+		InputEngine(int tickRate = 80);
 		void pollInput();
 		void pollKeyInput();
+		void pollKeysDown();
 		void pollMouseInput();
 		void poll();
 	private:
 		void isKeyPressed();
+		void initializeKeyMap();
+		void initializeKeyMapN();
 	};
 }
