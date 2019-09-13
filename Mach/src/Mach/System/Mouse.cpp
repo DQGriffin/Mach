ea073596aabba4ae2bf39f71dabdcab81e860144
D'Quavius J. Griffin <dquaviusgriffin@gmail.com>
2019-09-13 00:41:36 -0400

    Initial commit

---
+++ Mach/src/Mach/System/Mouse.cpp
@@ -0,0 +1,17 @@
+#include "Mouse.h"
+
+//==========================================================================
+// Fire a mouse click event
+//==========================================================================
+void Mach::Mouse::click(int x, int y, Mouse::Button button)
+{
+	// TODO: Add logic
+}
+
+//==========================================================================
+// Fire a mouse click event
+//==========================================================================
+void Mach::Mouse::click(Vector2i position, Mouse::Button button)
+{
+	// TODO: Add logic
+}
---
+++ Mach/src/Mach/System/Mouse.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "..//MachAPI.h"
+#include "..//Core/EventManager.h"
+#include "Vector2.h"
+
+namespace Mach
+{
+	class Mouse
+	{
+	public:
+		static enum Button {LeftButton, RightButton, MiddleButton, Extra1, Extra2};
+	public:
+		static void click(int x, int y, Button button = Button::LeftButton);
+		static void click(Vector2i position, Button button = Button::LeftButton);
+	};
+}
