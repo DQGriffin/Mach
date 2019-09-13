8e3e427236ba2db1e2285d2dd0462c11780d70b8
D'Quavius J. Griffin <dquaviusgriffin@gmail.com>
2019-09-13 00:43:44 -0400

    Implement MouseEvent

--- Mach/src/Mach/Core/Event.h
+++ Mach/src/Mach/Core/Event.h
@@ -1,19 +1,27 @@
 #pragma once
 #include "..//MachAPI.h"
 #include "..//System/Keyboard.h"
+#include "..//System/Mouse.h"
 
 namespace Mach
 {
 	class MACH_API Event
 	{
 	public:
-		enum class Type{KeyEvent};
+		enum class Type{KeyEvent, MouseEvent};
 
 		Type type;
 
 		struct KeyEvent
 		{
 			Keyboard::Key key;
+		};
+
+		struct MouseEvent
+		{
+			Mouse::Button button;
+			int x;
+			int y;
 		};
 
 		union
