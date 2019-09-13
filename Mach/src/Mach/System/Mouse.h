de55534ca2604f880fb2f86cd68e8bfa6d19a98e
D'Quavius J. Griffin <dquaviusgriffin@gmail.com>
2019-09-13 01:27:06 -0400

    Remove EventManager include

--- Mach/src/Mach/System/Mouse.h
+++ Mach/src/Mach/System/Mouse.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "..//MachAPI.h"
-#include "..//Core/EventManager.h"
+//#include "..//Core/EventManager.h"
 #include "Vector2.h"
 
 namespace Mach
@@ -8,7 +8,7 @@ namespace Mach
 	class Mouse
 	{
 	public:
-		static enum Button {LeftButton, RightButton, MiddleButton, Extra1, Extra2};
+		static enum Button { LeftButton, RightButton, MiddleButton, Extra1, Extra2 };
 	public:
 		static void click(int x, int y, Button button = Button::LeftButton);
 		static void click(Vector2i position, Button button = Button::LeftButton);
