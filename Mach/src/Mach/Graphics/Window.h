90ef53d3053b138b61ed744cfcaa6f122c1744d4
D'Quavius J. Griffin <dquaviusgriffin@gmail.com>
2019-09-17 00:45:55 -0400

    Add license header

--- Mach/src/Mach/Graphics/Window.h
+++ Mach/src/Mach/Graphics/Window.h
@@ -1,8 +1,33 @@
+////////////////////////////////////////////////////////////
+//
+// Mach Engine
+// Copyright (C) 2019 D'Quavius J. Griffin
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from ny source distribution.
+//
+////////////////////////////////////////////////////////////
+
 #pragma once
 #include <SFML/Graphics/RenderWindow.hpp>
 #include "..//MachAPI.h"
 #include "..//Logging/Logger.h"
 #include "..//System/Vector2.h"
+#include "Drawable.h"
 
 namespace Mach
 {
@@ -13,7 +38,7 @@ namespace Mach
 		Vector2i m_Size;
 		Vector2i m_Position;
 		std::string m_Title;
-		sf::RenderWindow m_RenderWindow;
+		sf::RenderWindow m_RenderWindow;		
 	public:
 		Window();
 		Window(int x, int y, int width, int height);
@@ -25,6 +50,7 @@ namespace Mach
 		void setSize(Vector2i size);
 		void setPosition(int x, int y);
 		void setPosition(Vector2i position);
+		void draw(Drawable& drawable);
 	private:
 	};
 }
