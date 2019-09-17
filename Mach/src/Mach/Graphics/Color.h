7eb5e8b1d5a35435d25cb3258abbf1549a371596
D'Quavius J. Griffin <dquaviusgriffin@gmail.com>
2019-09-16 22:32:28 -0400

    Implement operator overloads

--- Mach/src/Mach/Graphics/Color.h
+++ Mach/src/Mach/Graphics/Color.h
@@ -24,4 +24,65 @@ namespace Mach
 		Color();
 		Color(int r, int g, int b, int a = 255);
 	};
+
+	Color operator+(const Color& left, const Color& right)
+	{
+		Color result;
+		result.r = left.r + right.r;
+		result.g = left.g + right.g;
+		result.b = left.b + right.b;		
+		return result;
+	}
+
+	Color operator-(const Color& left, const Color& right)
+	{
+		Color result;
+		result.r = left.r - right.r;
+		result.g = left.g - right.g;
+		result.b = left.b - right.b;		
+		return result;
+	}
+
+	Color operator*(const Color& left, const Color& right)
+	{
+		Color result;
+		result.r = left.r * right.r;
+		result.g = left.g * right.g;
+		result.b = left.b * right.b;		
+		return result;
+	}
+
+	Color& operator+=(Color& left, const Color& right)
+	{
+		left.r += right.r;
+		left.g += right.g;
+		left.b += right.b;
+		return left;
+	}
+
+	Color& operator-=(Color& left, const Color& right)
+	{
+		left.r -= right.r;
+		left.g -= right.g;
+		left.b -= right.b;
+		return left;
+	}
+
+	Color& operator*=(Color& left, const Color& right)
+	{
+		left.r *= right.r;
+		left.g *= right.g;
+		left.b *= right.b;
+		return left;
+	}
+
+	bool operator==(const Color& left, const Color& right)
+	{
+		return (left.r == right.r) && (left.g == right.g) && (left.b == right.b) && (left.a == right.a);
+	}
+
+	bool operator!=(const Color& left, const Color& right)
+	{
+		return !operator==(left, right);
+	}
 }
