dce753f9191368d8acb3168e9e18e36bebd8d0ad
D'Quavius J. Griffin <dquaviusgriffin@gmail.com>
2019-09-18 18:21:52 -0400

    Add new includes

--- Mach/src/Graphics.h
+++ Mach/src/Graphics.h
@@ -1,3 +1,8 @@
 #pragma once
 #include "Mach/Graphics/FontManager.h"
 #include "Mach/Graphics/Color.h"
+#include "Mach/Graphics/Drawable.h"
+#include "Mach/Graphics/Renderer.h"
+#include "Mach/Graphics/RenderGroup.h"
+#include "Mach/Graphics/Text.h"
+#include "Mach/Graphics/Window.h"
