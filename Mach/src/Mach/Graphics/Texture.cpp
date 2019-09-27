648452b950bddbf658e796702b6d77ba9dd90953
D'Quavius J. Griffin <dquaviusgriffin@gmail.com>
2019-09-26 21:26:39 -0400

    Implement logic

--- Mach/src/Mach/Graphics/Texture.cpp
+++ Mach/src/Mach/Graphics/Texture.cpp
@@ -1,1 +1,64 @@
 #include "Texture.h"
+
+//==========================================================================
+// Texture constructor
+//==========================================================================
+Mach::Texture::Texture()
+{
+}
+
+//==========================================================================
+// Texture constructor
+//==========================================================================
+Mach::Texture::Texture(const std::string& path)
+{
+	loadFromFile(path);
+}
+
+//==========================================================================
+// Texture copy constructor
+//==========================================================================
+Mach::Texture::Texture(const Texture& other)
+{
+	m_Texture = other.m_Texture;
+}
+
+//==========================================================================
+// Copy assignment
+//==========================================================================
+void Mach::Texture::operator=(const Texture& other)
+{
+	m_Texture = other.m_Texture;
+}
+
+//==========================================================================
+// Create an empty texture with the given size
+//==========================================================================
+bool Mach::Texture::create(unsigned int width, unsigned int height)
+{
+	return m_Texture.create(width, height);
+}
+
+//==========================================================================
+// Load a texture from a file
+//==========================================================================
+bool Mach::Texture::loadFromFile(std::string path)
+{
+	return m_Texture.loadFromFile(path);
+}
+
+//==========================================================================
+// Get the size of the texture
+//==========================================================================
+Mach::Vector2i Mach::Texture::getSize()
+{
+	return Mach::Vector2i(m_Texture.getSize().x, m_Texture.getSize().y);
+}
+
+//==========================================================================
+// Get the underlying sf::Texture
+//==========================================================================
+sf::Texture* Mach::Texture::get()
+{
+	return &m_Texture;
+}
--- Mach/src/Mach/Graphics/Texture.h
+++ Mach/src/Mach/Graphics/Texture.h
@@ -1,10 +1,25 @@
 #pragma once
 #include "..//MachAPI.h"
+#include "..//System/Vector2.h"
+#include <SFML/Graphics/Texture.hpp>
+#include <string>
 
 namespace Mach
 {
 	class MACH_API Texture
 	{
-
+	public:
+	private:
+		sf::Texture m_Texture;
+	public:
+		Texture();
+		Texture(const std::string& path);
+		Texture(const Texture& other);
+		void operator=(const Texture& other);
+		bool create(unsigned int width, unsigned int height);
+		bool loadFromFile(std::string path);
+		Mach::Vector2i getSize();
+	protected:
+		sf::Texture* get();
 	};
 }
